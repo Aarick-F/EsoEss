d7fb9deeceb6cea3e05c4421a5dbd873b8e49317
Aarick-F <zeflyndesign@gmail.com>
2018-05-29 18:36:49 -0400

    ?

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB, "root", process.env.DB_PASSWORD, {
+const sequelize = new Sequelize("esoess_db", "root", "pleasework", {
   host: "localhost",
   port: 3306,
   dialect: "mysql"
